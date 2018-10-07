9952f0a2b31f72f4799006957e7fa40c524f2590
Ryan <ryan.holt@me.com>
2018-10-07 18:35:47 -0500

    Simplified calculations

--- Water Reservoir Calculator (Test).py
+++ Water Reservoir Calculator (Test).py
@@ -4,14 +4,14 @@ reservoir_volume = 4.445e8
 rainfall = 5e6
 
 # decrease the rainfall variable by 10% to account for runoff
-rainfall -= rainfall*(.10)
+rainfall -= .9
 # add the rainfall variable to the reservoir_volume variable
 reservoir_volume += rainfall
 # increase reservoir_volume by 5% to account for stormwater that flows
 # into the reservoir in the days following the storm
-reservoir_volume += reservoir_volume*.05
+reservoir_volume *= 1.05
 # decrease reservoir_volume by 5% to account for evaporation
-reservoir_volume -= (reservoir_volume*.05)
+reservoir_volume *= 0.95
 # subtract 2.5e5 cubic metres from reservoir_volume to account for water
 # that's piped to arid regions.
 reservoir_volume -= 2.5e5
