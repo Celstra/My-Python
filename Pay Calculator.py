5455e7237c4c96d5af45113a67110bc1d9c3aedf
Ryan <ryan.holt@me.com>
2018-10-30 20:18:25 +0800

    Added error catch when numerical value is not entered. Converted float.

--- Pay Calculator.py
+++ Pay Calculator.py
@@ -3,9 +3,16 @@
 hrs = input("Enter Hours:")
 rate = input("Rate Per Hour:")
 
-if float(hrs) <= 40:
-    pay = float(hrs) * float(rate)
-elif float(hrs) > 40:
-    pay = (40 * float(rate)) + ((float(hrs) - 40) * (float(rate) * 1.5))
+try:
+    f_hrs = float(hrs)
+    f_rate = float(rate)
+except:
+    print('Error, please enter numerical value')
+    quit()
+
+if f_hrs <= 40:
+    pay = f_hrs * f_rate
+elif f_hrs > 40:
+    pay = (40 * f_rate) + (f_hrs - 40) * (f_rate * 1.5)
 
 print("Pay: " ,pay)
