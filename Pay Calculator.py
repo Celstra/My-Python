bacf5bfd5c060adabb2e7ab17dff108a2da0d1b6
Ryan <ryan.holt@me.com>
2018-10-30 20:09:26 +0800

    Added a way to check for over time.

--- Pay Calculator.py
+++ Pay Calculator.py
@@ -3,5 +3,9 @@
 hrs = input("Enter Hours:")
 rate = input("Rate Per Hour:")
 
-pay = float(hrs) * float(rate)
+if float(hrs) <= 40:
+    pay = float(hrs) * float(rate)
+elif float(hrs) > 40:
+    pay = (40 * float(rate)) + ((float(hrs) - 40) * (float(rate) * 1.5))
+
 print("Pay: " ,pay)
