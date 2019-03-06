dc23f35b94bafca6f955a959397184a76f1ae221
Ryan <ryan.holt@me.com>
2019-03-06 09:47:43 -0600

    Initial commit. Counts sum of numbers.

---
+++ scaping_HTML_BS.py
@@ -0,0 +1,13 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+html=request.urlopen('http://py4e-data.dr-chuck.net/comments_151369.html').read()
+soup = BeautifulSoup(html)
+tags=soup('span')
+
+sum=0
+
+for tag in tags:
+    sum=sum+int(tag.contents[0])
+
+print(sum)
